258e02a955e270d6d68afb5f63f37911ffe25ee0
Keali'i Transfield <liitransfield@gmail.com>
2021-01-25 10:33:36 -0700

    Made Bouncy Ball Game and Scripts

---
+++ UVU_Truck_Game/Assets/Scripts/BallGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject ball;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            Instantiate(ball);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
