34db9c820d6ed138cd3b0ba45f137d7c646bb0c5
Keali'i Transfield <liitransfield@gmail.com>
2021-01-29 08:15:39 -0700

    Added Truck random color feature

--- UVU_Truck_Game/Assets/Scripts/TruckControls.cs
+++ UVU_Truck_Game/Assets/Scripts/TruckControls.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarControls : MonoBehaviour
+public class TruckControls : MonoBehaviour
 {
     // Sensitivity of the forward/backward controls
     [SerializeField] private int wheelSpeed = 10;
-    // Sensitivity of the car tilting (spinning) controls
+    // Sensitivity of the truck tilting (spinning) controls
     [SerializeField] private int tiltSensitivity = 25;
-    // List of car wheels
+    // Random color to choose from
+    [SerializeField] private Color[] colorOptions;
+    // List of truck wheels
     List<Rigidbody2D> wheels  = new List<Rigidbody2D>();
-    // The car's Rigidbody2D Component
+    // The truck's Rigidbody2D Component
     private Rigidbody2D _myRigidbody2D;
 
     // Start Function is called before the first frame update
     // (or at the gameObject's creation/reactivation)
     void Start()
     {
-        // Loop through the car's children
+        // Loop through the truck's children
         foreach (Transform child in transform)
         {
             // Find the wheels in the children of the children
@@ -27,12 +29,17 @@ public class CarControls : MonoBehaviour
                 if (child2.CompareTag("Wheel"))
                 {
                     // Get the wheel Rigidbody2D Component, and add it to the wheels list
-                    wheels.Add(child2.gameObject.GetComponent<Rigidbody2D>());
+                    wheels.Add(child2.GetComponent<Rigidbody2D>());
                 }
+                else if (child2.name == "ColoredPiece")
+                {
+                    child2.GetComponent<SpriteRenderer>().color = colorOptions[Random.Range(0, colorOptions.Length)];
+                }
+
             }
         }
-        // Get the car's Rigidbody2D Component
-        _myRigidbody2D = transform.GetComponent<Rigidbody2D>();
+        // Get the truck's Rigidbody2D Component
+            _myRigidbody2D = transform.GetComponent<Rigidbody2D>();
     }
 
     // Update Function is called once per frame
@@ -58,7 +65,7 @@ public class CarControls : MonoBehaviour
             // Set up the rotation variable in preparation for applied angular force
             // (Multiply Axis by sensitivity variable)
             float rotation = Input.GetAxis("Horizontal") * -tiltSensitivity;
-            // Turn (tilt) the whole car
+            // Turn (tilt) the whole truck
             _myRigidbody2D.AddTorque(rotation);
         }
     }
