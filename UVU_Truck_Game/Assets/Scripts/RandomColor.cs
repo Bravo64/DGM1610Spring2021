5c4b22ee97a38b4ea6c4bd113d5a760624177c8e
Keali'i Transfield <liitransfield@gmail.com>
2021-01-30 16:29:05 -0700

    Made it so Truck's rigidbody is disable when fuel runs out

--- UVU_Truck_Game/Assets/Scripts/RandomColor.cs
+++ UVU_Truck_Game/Assets/Scripts/RandomColor.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class RandomColor : MonoBehaviour
 {
-    // Random color to choose from
-    [SerializeField] private Color[] colorOptions;
+    /*
+    ---------------- Documentation ---------------------
 
-    // Start is called before the first frame update
-    void Update()
+    Script's Name: RandomColor.cs
+
+    Script's Description: This script assigns a random color
+        to the sprite that it is attached to. In our case, we
+        are using it to give the front end of the truck a random
+        color on start. The random color is selected from an 
+        array of random colors that are set up in the editor.
+        
+    Script's Methods:
+        - Start
+        - Update
+
+    --------------------- DOC END ----------------------
+     */
+    
+    //----------------- Variables ---------------------
+    
+    // Array of random colors to choose from (Set up in editor)
+    [SerializeField] private Color[] colorOptions;
+    // The sprite renderer of this object
+    private SpriteRenderer _myRenderer;
+    
+    //-------------------------------------------------
+    
+    //-------------- The Start Method -------------------
+    // This Method is called before the first frame update
+    // (or at the gameObject's creation/reactivation). In
+    // this script, it deals with getting the sprite renderer
+    // and assigning a random color to the sprite.
+    //-----------------------------------------------------
+    void Start()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-            GetComponent<SpriteRenderer>().color = colorOptions[Random.Range(0, colorOptions.Length)];
+        // Grab my sprite renderer.
+        _myRenderer = GetComponent<SpriteRenderer>();
+        // Pick a random color from the array of colors,
+        // and assign it to the sprite
+        _myRenderer.color = colorOptions[Random.Range(0, colorOptions.Length)];
     }
 }
