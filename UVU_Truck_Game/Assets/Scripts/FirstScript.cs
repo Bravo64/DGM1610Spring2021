08e2a005f55b28bdb33db5761f72000d2e970cb4
Keali'i Transfield <liitransfield@gmail.com>
2021-02-01 22:16:28 -0700

    Edited FirstScript a tiny bit

--- UVU_Truck_Game/Assets/Scripts/FirstScript.cs
+++ UVU_Truck_Game/Assets/Scripts/FirstScript.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class FirstScript : MonoBehaviour
 {
+    // Public variables can be changed in the editor
+    public string myName;
+    
     // Start is called before the first frame update
     void Start()
     {
         // Print a message to the console
         Debug.Log("Hello Beautiful World!");
+        Debug.Log("My name is" + myName);
     }
 
     // Update is called once per frame
