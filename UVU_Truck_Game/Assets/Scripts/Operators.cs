fae5ba1b32f17ca0c966bde42b82c51f4074a73b
Keali'i Transfield <liitransfield@gmail.com>
2021-02-10 17:03:36 -0700

    Made a tiny change to the operators script

---
+++ UVU_Truck_Game/Assets/Scripts/Operators.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Operators : MonoBehaviour
+{
+    
+    private int _integerVariable = 123;
+    private double _doubleVariable = 19.99;
+    private char _characterVariable = 'A';
+    private string _stringVariable = "Hello World";
+    private bool _booleanVariable = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(_integerVariable);
+        print(_doubleVariable);
+        print(_characterVariable);
+        print(_stringVariable);
+        print(_booleanVariable);
+
+        _integerVariable++;
+        _doubleVariable -= 1.0f;
+        _characterVariable = 'B';
+        _stringVariable += "!";
+        _booleanVariable = !_booleanVariable
+        
+        print(_integerVariable);
+        print(_doubleVariable);
+        print(_characterVariable);
+        print(_stringVariable);
+        print(_booleanVariable);
+    }
+    
+}
