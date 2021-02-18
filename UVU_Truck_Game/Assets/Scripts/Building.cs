b1e260a3615003f22703e07fb2dce97b50f74b35
Keali'i Transfield <liitransfield@gmail.com>
2021-02-17 19:35:52 -0700

    Began the creation of a building designed to explode when triggered

---
+++ UVU_Truck_Game/Assets/Scripts/Building.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Vehicle"))
+        {
+            Debug.Log("Vehicle Detected");
+        }
+    }
+}
