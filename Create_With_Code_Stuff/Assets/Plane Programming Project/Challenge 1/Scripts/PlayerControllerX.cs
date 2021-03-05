4e7f6e21cf0bde381a8cebdfc5068df07a79b52f
Keali'i Transfield <liitransfield@gmail.com>
2021-03-04 22:00:26 -0700

    Programmed two plane weapons with instantiated bullet projectiles

--- Create_With_Code_Stuff/Assets/Plane Programming Project/Challenge 1/Scripts/PlayerControllerX.cs
+++ Create_With_Code_Stuff/Assets/Plane Programming Project/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
-    public float propRotationSpeed = 720.0f;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float rotationSpeed;
+    [SerializeField]
+    private float propRotationSpeed = 720.0f;
+    [SerializeField] 
+    private float weaponForce = 100.0f;
     [SerializeField]
     private Transform propeller;
+    [SerializeField]
+    private Rigidbody bullet;
+    [SerializeField]
+    private Transform weaponLeft;
+    [SerializeField]
+    private Transform weaponRight;
     private float verticalInput;
     private float horizontalInput;
 
@@ -33,5 +44,13 @@ public class PlayerControllerX : MonoBehaviour
         transform.Rotate(Vector3.right * verticalInput * rotationSpeed * Time.deltaTime);
         
         propeller.transform.Rotate(Vector3.forward * propRotationSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Rigidbody bulletRB1 = Instantiate(bullet, weaponLeft.position, weaponLeft.rotation);
+            bulletRB1.velocity = transform.forward * weaponForce;
+            Rigidbody bulletRB2 = Instantiate(bullet, weaponRight.position, weaponRight.rotation);
+            bulletRB2.velocity = transform.forward * weaponForce;
+        }
     }
 }
