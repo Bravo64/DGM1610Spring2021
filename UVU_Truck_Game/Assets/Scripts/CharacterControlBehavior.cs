7ee20db6af30f12fddc5ce4465006cb5df2e60cf
Keali'i Transfield <liitransfield@gmail.com>
2021-03-05 18:41:18 -0700

    Added a test scene for testing out a character controller.

---
+++ UVU_Truck_Game/Assets/Scripts/CharacterControlBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterControlBehavior : MonoBehaviour
+{
+    public float speed = 3f, gravity = -20f, jumpForce = 100f;
+
+    private CharacterController controller;
+    private Vector3 movement, rotation;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.Set(speed*Input.GetAxis("Vertical"), gravity, 0);
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            movement.y += jumpForce;
+        }
+        rotation.y = Input.GetAxis("Horizontal");
+        transform.Rotate(rotation);
+        movement = transform.TransformDirection(movement);
+        controller.Move(movement * Time.deltaTime);
+    }
+}
