4e3f9ffe159e4581c44bf8be3d4778ed8e2f09a6
Keali'i Transfield <liitransfield@gmail.com>
2021-03-09 20:29:59 -0700

    Experimented with making the test character controller jump smoothly

--- UVU_Truck_Game/Assets/Scripts/CharacterControlBehavior.cs
+++ UVU_Truck_Game/Assets/Scripts/CharacterControlBehavior.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class CharacterControlBehavior : MonoBehaviour
 {
-    public float speed = 3f, gravity = -20f, jumpForce = 100f;
+    public float speed = 3f, setGravity = -20f, jumpForce = 100f;
 
+    private float currentGravity = -20f;
     private CharacterController controller;
     private Vector3 movement, rotation;
     
@@ -15,15 +16,21 @@ public class CharacterControlBehavior : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        currentGravity = setGravity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        movement.Set(speed*Input.GetAxis("Vertical"), gravity, 0);
+        movement.Set(speed*Input.GetAxis("Vertical"), currentGravity, 0);
         if (Input.GetButtonDown("Jump") && controller.isGrounded)
         {
+            currentGravity = 0;
             movement.y += jumpForce;
+        }
+        if (currentGravity > setGravity)
+        {
+            currentGravity += setGravity * Time.deltaTime;
         }
         rotation.y = Input.GetAxis("Horizontal");
         transform.Rotate(rotation);
