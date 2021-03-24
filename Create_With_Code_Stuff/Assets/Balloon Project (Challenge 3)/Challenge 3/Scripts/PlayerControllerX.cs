cf5e2138bee026f6d88e17802e2a55d4c1f25420
Keali'i Transfield <liitransfield@gmail.com>
2021-03-23 20:27:08 -0600

    Completed "Create with Code" Balloon challenge (and side scrolling jump)

---
+++ Create_With_Code_Stuff/Assets/Balloon Project (Challenge 3)/Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameEvents;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver;
+
+    public float floatForce, groundBounceForce;
+    public VoidEvent reloadSceneEvent;
+    private float gravity = -4.0f;
+    private Rigidbody playerRb;
+    private MeshRenderer playerRenderer;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+    public AudioClip bounceSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 currentGravity = Physics.gravity;
+        currentGravity.y = gravity;
+        Physics.gravity = currentGravity;
+        playerRb = GetComponent<Rigidbody>();
+        playerRenderer = GetComponent<MeshRenderer>();
+        playerAudio = GetComponent<AudioSource>();
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 2, ForceMode.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * floatForce);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            other.gameObject.SetActive(false);
+            StartCoroutine(Dead());
+        } 
+
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            other.gameObject.SetActive(false);
+
+        }
+        
+        else if (other.gameObject.CompareTag("Ground"))
+        {
+            playerRb.AddForce(Vector3.up * groundBounceForce, ForceMode.Impulse);
+            playerAudio.PlayOneShot(bounceSound);
+        }
+
+    }
+
+    IEnumerator Dead()
+    {
+        playerRenderer.enabled = false;
+        playerRb.constraints = RigidbodyConstraints.FreezeAll;
+        explosionParticle.Play();
+        playerAudio.PlayOneShot(explodeSound, 1.0f);
+        gameOver = true;
+        Debug.Log("Game Over!");
+        yield return new WaitForSeconds(3.0f);
+        reloadSceneEvent.Raise();
+    }
+
+}
--- Create_With_Code_Stuff/Assets/Side-Scrolling Project (Jump Force Lesson)/Scripts/PlayerController.cs
+++ Create_With_Code_Stuff/Assets/Side-Scrolling Project (Jump Force Lesson)/Scripts/PlayerController.cs
@@ -1,18 +1,103 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using GameEvents;
 using UnityEngine;
+using Random = UnityEngine.Random;
+using Vector3 = UnityEngine.Vector3;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AudioSource))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private float jumpForce = 10.0f;
+    [SerializeField]
+    private AudioClip jumpSound;
+    [SerializeField]
+    private AudioClip deathSound;
+    [SerializeField]
+    private SkinnedMeshRenderer myMeshRenderer;
+    [SerializeField]
+    private Rigidbody deadPlayerRigidbody;
+    [SerializeField]
+    private ParticleSystem runParticle;
+    [SerializeField]
+    private ParticleSystem jumpParticle;
+    [SerializeField]
+    private ParticleSystem deathParticle;
+    [SerializeField]
+    private VoidEvent reloadScene;
+    
+    private Rigidbody _myRigidbody;
+    private Animator _myAnimator;
+    private AudioSource _myAudioSource;
+    private bool _isGrounded = false;
+    private bool _deathSetupComplete = false;
+    
     void Start()
     {
-        
+        _myRigidbody = GetComponent<Rigidbody>();
+        _myAnimator = GetComponent<Animator>();
+        _myAudioSource = GetComponent<AudioSource>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _myRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumpParticle.Play();
+            runParticle.Stop();
+            _isGrounded = false;
+            if (!_myAudioSource.isPlaying)
+            {
+                _myAudioSource.pitch = Random.Range(0.5f, 0.75f);
+                _myAudioSource.PlayOneShot(jumpSound);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = true;
+            _myAnimator.SetBool("Grounded", true);
+            runParticle.Play();
+        }
+
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            StartCoroutine(Dead());
+        }
+    }
+    
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = false;
+            _myAnimator.SetBool("Grounded", false);
+        }
+    }
+
+    IEnumerator Dead()
+    {
+        if (!_deathSetupComplete)
+        {
+            myMeshRenderer.enabled = false;
+            _myAudioSource.pitch = 1.0f;
+            _myAudioSource.PlayOneShot(deathSound);
+            deadPlayerRigidbody.gameObject.SetActive(true);
+            deadPlayerRigidbody.AddTorque(Random.Range(-360, 360), Random.Range(-360, 360), Random.Range(-360, 360));
+            deadPlayerRigidbody.AddForce((transform.up - transform.forward) * 9.0f);
+            deathParticle.Play();
+            _deathSetupComplete = true;
+            yield return new WaitForSeconds(2.0f);
+            reloadScene.Raise();
+        }
     }
 }
