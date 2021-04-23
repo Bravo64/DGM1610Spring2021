94e33b56f5862e1842a2accbccd8458542c1d365
Keali'i Transfield <liitransfield@gmail.com>
2021-04-23 11:19:07 -0600

    Added some new Action Events to the TOOLS Package

--- TOOLS_Package_Setup/Assets/1. TOOLS/Setup/Scriptable Object Setup/Variable Data/StateEnumData.cs
+++ TOOLS_Package_Setup/Assets/1. TOOLS/Setup/Scriptable Object Setup/Variable Data/StateEnumData.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New State Enum Variable", menuName = "Variable/State Enum")]
@@ -5,4 +6,32 @@ public class StateEnumData : ScriptableObject
 {
     public enum State{ state0, state1, state2, state3, state4, state5}
     public State value = State.state0;
+    
+    public void SetEnumState(int input)
+    {
+        switch (input)
+        {
+            case 0:
+                value = State.state0;
+                break;
+            case 1:
+                value = State.state1;
+                break;
+            case 2:
+                value = State.state2;
+                break;
+            case 3:
+                value = State.state3;
+                break;
+            case 4:
+                value = State.state4;
+                break;
+            case 5:
+                value = State.state5;
+                break;
+            default:
+                value = State.state0;
+                break;
+        }
+    }
 }
