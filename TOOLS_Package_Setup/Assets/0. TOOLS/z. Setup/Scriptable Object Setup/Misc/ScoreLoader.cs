09bd41dfc839faaad58f4c6d342478d58b50cabd
Keali'i Transfield <liitransfield@gmail.com>
2021-05-07 22:54:48 -0600

    Made "level" Score Loaders that attach the scene index to the save key.

--- TOOLS_Package_Setup/Assets/0. TOOLS/z. Setup/Scriptable Object Setup/Misc/ScoreLoader.cs
+++ TOOLS_Package_Setup/Assets/0. TOOLS/z. Setup/Scriptable Object Setup/Misc/ScoreLoader.cs
@@ -1,18 +1,39 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [CreateAssetMenu(fileName = "Score Loader Obj", menuName = "Misc/Score Loader Obj")]
 public class ScoreLoader : ScriptableObject
 {
     public IntData scoreIntDataObj;
+    public bool attachSceneIndexToKey;
     public string scoreSaveKey;
+    
+    private string _completeKey;
     
     public void SaveScore()
     {
-        PlayerPrefs.SetInt (scoreSaveKey, scoreIntDataObj.value);
+        if (attachSceneIndexToKey)
+        {
+            _completeKey = scoreSaveKey + SceneManager.GetActiveScene().buildIndex.ToString();
+        }
+        else
+        {
+            _completeKey = scoreSaveKey;
+        }
+        PlayerPrefs.SetInt (_completeKey, scoreIntDataObj.value);
     }
     
     public void LoadScore()
     {
-        scoreIntDataObj.value = PlayerPrefs.GetInt(scoreSaveKey, 0);
+        if (attachSceneIndexToKey)
+        {
+            _completeKey = scoreSaveKey + SceneManager.GetActiveScene().buildIndex.ToString();
+        }
+        else
+        {
+            _completeKey = scoreSaveKey;
+        }
+        PlayerPrefs.SetInt (scoreSaveKey, scoreIntDataObj.value);
+        scoreIntDataObj.value = PlayerPrefs.GetInt(_completeKey, 0);
     }
 }
